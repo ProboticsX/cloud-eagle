59055507a052afa8194ce6e429196918ec39ec2b
ProboticsX <jainshubham0403@gmail.com>
2026-04-16 15:36:00 +0530

    add answer 4

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,1 +1,12 @@
 # Deployment script here
+
+# Rolling Deployment:
+# - If the environment is qa/staging then the script would perform a rolling deployment.
+# - It would first stop the current container, then start the new container.
+# - It would then wait for the new container to be healthy, and then start the next container.
+# - It would repeat this process until all containers are deployed.
+
+# Blue/Green Deployment:
+# - If the environment is prod then the script would perform a blue/green deployment. 
+# - Let's say the current traffic is being served by "Blue" env.
+# - It would first stop the current container in the "Green" environment, then start the new container.
--- scripts/smoke_test.py
+++ scripts/smoke_test.py
@@ -1,1 +1,10 @@
 # Smoke Tests here
+
+# Tests fail:
+# - If the env is qa, staging: then the script would perform a rolling rollback.
+# - If the env is prod: then the script would perform a blue/green rollback.
+# - Refer rollback.py
+
+# Tests Pass:
+# - If the env is qa, staging: do nothing, the pipeline updates the latest image tag in S3.
+# - If the env is prod: then the script would switch traffic to the environment running the new image in a full or partial rollout strategy using the ALB switch.
